bab2e76dca2ede2f75fd23481b4bd35009117b88
kenjinote <2605401+kenjinote@users.noreply.github.com>
2023-02-12 12:39:04 +0900

    initial commit

---
+++ resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by res.rc
+//
+#define IDR_MENU1                       101
+#define IDM_MENU1                       40001
+#define IDM_MENU2                       40002
+#define IDM_MENU3                       40003
+#define IDM_END                         40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
